f023369e16d7bbaa5f75b84e37babcbbc41cfe29
Tanguy ROUDAUT <tanguy@roudaut.dev>
2026-02-27 19:49:47 +0100

    test: remove useless tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -22,27 +22,3 @@ fn test_multi_stylesheet_generation() {
         "tests/yaml_stylesheets/dyn.yaml"
     ]);
 }
-
-#[test]
-fn test_all_states() {
-    assert_stylesheet!("tests/yaml_stylesheets/all_states.yaml");
-}
-
-#[test]
-fn test_all_properties() {
-    assert_stylesheets!(&[
-        "tests/yaml_stylesheets/properties_arc.yaml",
-        "tests/yaml_stylesheets/properties_background.yaml",
-        "tests/yaml_stylesheets/properties_border.yaml",
-        "tests/yaml_stylesheets/properties_flex.yaml",
-        "tests/yaml_stylesheets/properties_grid.yaml",
-        "tests/yaml_stylesheets/properties_image.yaml",
-        "tests/yaml_stylesheets/properties_line.yaml",
-        "tests/yaml_stylesheets/properties_margin.yaml",
-        "tests/yaml_stylesheets/properties_miscellaneous.yaml",
-        "tests/yaml_stylesheets/properties_outline.yaml",
-        "tests/yaml_stylesheets/properties_padding.yaml",
-        "tests/yaml_stylesheets/properties_size_and_position.yaml",
-        "tests/yaml_stylesheets/properties_text.yaml",
-    ]);
-}
