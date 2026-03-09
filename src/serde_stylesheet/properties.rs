80d7902f3d49bb699c76a0159ead138f0ad600e4
Tanguy ROUDAUT <tanguy@roudaut.dev>
2026-03-09 19:08:48 +0100

    chore: sort properties alphabetically

--- src/serde_stylesheet/properties.rs
+++ src/serde_stylesheet/properties.rs
@@ -89,125 +89,125 @@ macro_rules! make_properties {
 }
 
 make_properties! (
-    { width, i32 }
-    { min_width, i32 }
-    { max_width, i32 }
-    { height, i32 }
-    { min_height, i32 }
-    { max_height, i32 }
-    { length, i32 }
-    { x, i32 }
-    { y, i32 }
     { align, LVAlign }
-    { transform_width, i32 }
-    { transform_height, i32 }
-    { translate_x, i32 }
-    { translate_y, i32 }
-    { translate_radial, i32 }
-    { transform_scale_x, i32 }
-    { transform_scale_y, i32 }
-    { transform_rotation, i32 }
-    { transform_pivot_x, i32 }
-    { transform_pivot_y, i32 }
-    { transform_skew_x, i32 }
-    { transform_skew_y, i32 }
-    { pad_top, i32 }
-    { pad_bottom, i32 }
-    { pad_left, i32 }
-    { pad_right, i32 }
-    { pad_row, i32 }
-    { pad_column, i32 }
-    { pad_radial, i32 }
-    { margin_top, i32 }
+    // { anim, not_yet_implemented }
+    // { anim_duration, not_yet_implemented }
+    { arc_color, LVColor}
+    // { arc_image_src, not_yet_implemented }
+    { arc_opa, LVOpa }
+    { arc_rounded, bool }
+    { arc_width, i32 }
+    { base_dir, LVBaseDir }
+    { bg_color, LVColor }
+    // { bg_grad, not_yet_implemented }
+    { bg_grad_color, LVColor }
+    { bg_grad_dir, LVGradDir }
+    { bg_grad_opa, LVOpa }
+    { bg_grad_stop, i32 }
+    { bg_image_opa, LVOpa }
+    { bg_image_recolor, LVColor }
+    { bg_image_recolor_opa, LVOpa }
+    // { bg_image_src, not_yet_implemented }
+    { bg_image_tiled, bool }
+    { bg_main_opa, LVOpa }
+    { bg_main_stop, i32 }
+    { bg_opa, LVOpa }
+    // { bitmap_mask_src, not_yet_implemented }
+    { blend_mode, LVBlendMode }
+    { border_color, LVColor }
+    { border_opa, LVOpa }
+    { border_post, bool }
+    { border_side, LVBorderSide }
+    { border_width, i32 }
+    { clip_corner, bool }
+    // { color_filter_dsc, not_yet_implemented }
+    { color_filter_opa, LVOpa }
+    { flex_cross_place, LVFlexAlign }
+    { flex_flow, LVFlexFlow }
+    { flex_grow, i32 }
+    { flex_main_place, LVFlexAlign }
+    { flex_track_place, LVFlexAlign }
+    { grid_cell_column_pos, i32 }
+    { grid_cell_column_span, i32 }
+    { grid_cell_row_pos, i32 }
+    { grid_cell_row_span, i32 }
+    { grid_cell_x_align, LVGridAlign }
+    { grid_cell_y_align, LVGridAlign }
+    { grid_column_align, LVGridAlign }
+    { grid_column_dsc_array, LVGridDscArray }
+    { grid_row_align, LVGridAlign }
+    { grid_row_dsc_array, LVGridDscArray }
+    { height, i32 }
+    { image_colorkey, LVImageColorkey }
+    { image_opa, LVOpa }
+    { image_recolor, LVColor }
+    { image_recolor_opa, LVOpa }
+    { layout, LVLayout }
+    { length, i32 }
+    { line_color, LVColor }
+    { line_dash_gap, i32 }
+    { line_dash_width, i32 }
+    { line_opa, LVOpa }
+    { line_rounded, bool }
+    { line_width, i32 }
     { margin_bottom, i32 }
     { margin_left, i32 }
     { margin_right, i32 }
-    { bg_color, LVColor}
-    { bg_opa, LVOpa}
-    { bg_grad_color, LVColor}
-    { bg_grad_dir, LVGradDir}
-    { bg_main_stop, i32}
-    { bg_grad_stop, i32}
-    { bg_main_opa, LVOpa}
-    { bg_grad_opa, LVOpa}
-    // { bg_grad, not_yet_implemented }
-    // { bg_image_src, not_yet_implemented }
-    { bg_image_opa, LVOpa}
-    { bg_image_recolor, LVColor}
-    { bg_image_recolor_opa, LVOpa}
-    { bg_image_tiled, bool}
-    { border_color, LVColor}
-    { border_opa, LVOpa}
-    { border_width, i32}
-    { border_side, LVBorderSide}
-    { border_post, bool}
-    { outline_width, i32}
-    { outline_color, LVColor}
-    { outline_opa, LVOpa}
-    { outline_pad, i32}
-    { shadow_width, i32}
-    { shadow_offset_x, i32}
-    { shadow_offset_y, i32}
-    { shadow_spread, i32}
-    { shadow_color, LVColor}
-    { shadow_opa, LVOpa}
-    { image_opa, LVOpa}
-    { image_recolor, LVColor}
-    { image_recolor_opa, LVOpa}
-    { image_colorkey, LVImageColorkey}
-    { line_width, i32}
-    { line_dash_width, i32}
-    { line_dash_gap, i32}
-    { line_rounded, bool}
-    { line_color, LVColor}
-    { line_opa, LVOpa}
-    { arc_width, i32}
-    { arc_rounded, bool}
-    { arc_color, LVColor}
-    { arc_opa, LVOpa}
-    // { arc_image_src, not_yet_implemented }
-    { text_color, LVColor}
-    { text_opa, LVOpa}
+    { margin_top, i32 }
+    { max_height, i32 }
+    { max_width, i32 }
+    { min_height, i32 }
+    { min_width, i32 }
+    { opa, LVOpa }
+    { opa_layered, LVOpa }
+    { outline_color, LVColor }
+    { outline_opa, LVOpa }
+    { outline_pad, i32 }
+    { outline_width, i32 }
+    { pad_bottom, i32 }
+    { pad_column, i32 }
+    { pad_left, i32 }
+    { pad_radial, i32 }
+    { pad_right, i32 }
+    { pad_row, i32 }
+    { pad_top, i32 }
+    { radial_offset, i32 }
+    { radius, i32 }
+    { recolor, LVColor }
+    { recolor_opa, LVOpa }
+    { rotary_sensitivity, i32 }
+    { shadow_color, LVColor }
+    { shadow_offset_x, i32 }
+    { shadow_offset_y, i32 }
+    { shadow_opa, LVOpa }
+    { shadow_spread, i32 }
+    { shadow_width, i32 }
+    { text_align, LVTextAlign }
+    { text_color, LVColor }
+    { text_decor, LVTextDecor }
     // { text_font, not_yet_implemented }
-    { text_letter_space, i32}
-    { text_line_space, i32}
-    { text_decor, LVTextDecor}
-    { text_align, LVTextAlign}
-    { text_outline_stroke_color, LVColor}
-    { text_outline_stroke_width, i32}
-    { text_outline_stroke_opa, LVOpa}
-    { radius, i32}
-    { radial_offset, i32}
-    { clip_corner, bool}
-    { opa, LVOpa}
-    { opa_layered, LVOpa}
-    // { color_filter_dsc, not_yet_implemented }
-    { color_filter_opa, LVOpa}
-    { recolor, LVColor}
-    { recolor_opa, LVOpa}
-    // { anim, not_yet_implemented }
-    // { anim_duration, not_yet_implemented }
+    { text_letter_space, i32 }
+    { text_line_space, i32 }
+    { text_opa, LVOpa }
+    { text_outline_stroke_color, LVColor }
+    { text_outline_stroke_opa, LVOpa }
+    { text_outline_stroke_width, i32 }
+    { transform_height, i32 }
+    { transform_pivot_x, i32 }
+    { transform_pivot_y, i32 }
+    { transform_rotation, i32 }
+    { transform_scale_x, i32 }
+    { transform_scale_y, i32 }
+    { transform_skew_x, i32 }
+    { transform_skew_y, i32 }
+    { transform_width, i32 }
     // { transition, not_yet_implemented }
-    { blend_mode, LVBlendMode}
-    { layout, LVLayout}
-    { base_dir, LVBaseDir}
-    // { bitmap_mask_src, not_yet_implemented }
-    { rotary_sensitivity, i32}
-    { flex_flow, LVFlexFlow}
-    { flex_main_place, LVFlexAlign}
-    { flex_cross_place, LVFlexAlign}
-    { flex_track_place, LVFlexAlign}
-    { flex_grow, i32}
-    { grid_column_dsc_array, LVGridDscArray}
-    { grid_column_align, LVGridAlign}
-    { grid_row_dsc_array, LVGridDscArray}
-    { grid_row_align, LVGridAlign}
-    { grid_cell_column_pos, i32}
-    { grid_cell_x_align, LVGridAlign}
-    { grid_cell_column_span, i32}
-    { grid_cell_row_pos, i32}
-    { grid_cell_y_align, LVGridAlign}
-    { grid_cell_row_span, i32}
+    { translate_radial, i32 }
+    { translate_x, i32 }
+    { translate_y, i32 }
+    { width, i32 }
+    { x, i32 }
+    { y, i32 }
 );
 
 #[cfg(test)]
