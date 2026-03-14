bf98b2f4cf8cece2dad383010d3cc06382612058
Tanguy ROUDAUT <tanguy@roudaut.dev>
2026-03-14 15:49:01 +0100

    test: fix grid lvgl rendering test

--- tests_lvgl_rendering/src/test_cases/test_grid/test_grid.cpp
+++ tests_lvgl_rendering/src/test_cases/test_grid/test_grid.cpp
@@ -7,22 +7,21 @@
 
 static lv_obj_t* create_grid_container(lv_obj_t* parent) {
     lv_obj_t* container = lv_obj_create(parent);
-    lv_obj_set_size(container, 350, 350);
+    lv_obj_set_size(container, LV_PCT(90), LV_PCT(90));
     lv_obj_center(container);
+
+    SET_GRID_STYLE(container, ENUM_GRID_STYLE_TEST_GRID_LAYOUT);
+
     return container;
 }
 
 TEST("test_grid_layout") {
     lv_obj_t* container = create_grid_container(lv_screen_active());
-    SET_GRID_STYLE(container, ENUM_GRID_STYLE_TEST_GRID_LAYOUT);
 
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 9; i++) {
         lv_obj_t* child = lv_obj_create(container);
-        lv_obj_set_size(child, 80, 80);
-        lv_obj_set_style_grid_cell_column_pos(child, i % 3, LV_PART_MAIN);
-        lv_obj_set_style_grid_cell_row_pos(child, i / 3, LV_PART_MAIN);
-        lv_obj_set_style_grid_cell_column_span(child, 1, LV_PART_MAIN);
-        lv_obj_set_style_grid_cell_row_span(child, 1, LV_PART_MAIN);
+        lv_obj_set_grid_cell(child, LV_GRID_ALIGN_STRETCH, i % 3, 1,
+            LV_GRID_ALIGN_STRETCH, i / 3, 1);
     }
 
     TEST_CHECK(lv_obj_get_style_grid_column_align(container, LV_PART_MAIN) == LV_GRID_ALIGN_START);
@@ -41,7 +40,7 @@ TEST("test_grid_layout") {
 
     lv_obj_clear_and_set_state(container, LV_STATE_USER_3);
     TEST_CHECK(lv_obj_get_style_grid_column_align(container, LV_PART_MAIN) == LV_GRID_ALIGN_SPACE_EVENLY);
-    TEST_CHECK(lv_obj_get_style_grid_row_align(container, LV_PART_MAIN)    == LV_GRID_ALIGN_SPACE_BETWEEN);
+    TEST_CHECK(lv_obj_get_style_grid_row_align(container, LV_PART_MAIN)    == LV_GRID_ALIGN_SPACE_EVENLY);
     TEST_CHECK_SCREENSHOT_COMPARE("user_3");
 }
 
@@ -51,15 +50,6 @@ TEST("test_grid_cell") {
     lv_obj_t* obj = lv_obj_create(container);
     lv_obj_set_size(obj, 80, 80);
     SET_GRID_STYLE(obj, ENUM_GRID_STYLE_TEST_GRID_CELL);
-
-    for (int i = 1; i < 6; i++) {
-        lv_obj_t* child = lv_obj_create(container);
-        lv_obj_set_size(child, 80, 80);
-        lv_obj_set_style_grid_cell_column_pos(child, i % 3, LV_PART_MAIN);
-        lv_obj_set_style_grid_cell_row_pos(child, i / 3, LV_PART_MAIN);
-        lv_obj_set_style_grid_cell_column_span(child, 1, LV_PART_MAIN);
-        lv_obj_set_style_grid_cell_row_span(child, 1, LV_PART_MAIN);
-    }
 
     TEST_CHECK(lv_obj_get_style_grid_cell_column_pos(obj, LV_PART_MAIN)  == 0);
     TEST_CHECK(lv_obj_get_style_grid_cell_row_pos(obj, LV_PART_MAIN)     == 0);
